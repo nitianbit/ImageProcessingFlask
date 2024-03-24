164478110203a214ad0542780f3acc6fc27cfcd2
tushar <tushar.mangla1120@gmail.com>
2024-03-25 02:26:54 +0530

    add constant file for folder and file path

---
+++ utils/constant.py
@@ -0,0 +1,9 @@
+input_folder_path_constant = "/NitianBit/PROCESSEDIMAGES/input"
+output_folder_path_constant = "/NitianBit/PROCESSEDIMAGES/output"
+temp_folder_path_constant = "/NitianBit/PROCESSEDIMAGES/temp"
+failed_folder_path_constant = "/NitianBit/PROCESSEDIMAGES/failed"
+temp_bcode_folder_path_constant = "/NitianBit/PROCESSEDIMAGES/temp_bcode"
+barcode_txt_path_constant = "/NitianBit/PROCESSEDIMAGES/barcode.txt"
+current_date_barcode_txt_path_constant = "/NitianBit/PROCESSEDIMAGES"
+current_iteration_barcode_txt_path_constant = "/NitianBit/PROCESSEDIMAGES"
+
